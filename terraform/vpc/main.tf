8d034305f7118528945c66e92251be3d2a8c4345
Amimul-Basher <mdbasher.web@gmail.com>
2025-06-17 23:47:40 -0400

    S3 bucket added

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "s3backendamimul-tfstatefile"  # Use a globally unique name
+    key            = "vpcactions/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
